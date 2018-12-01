acebab1ba856e91081196924394c548367e9950f
= <00103017@uca.edu.sv>
2018-12-01 14:09:04 -0600

    listo el modelo y la conexion mongodb

---
+++ models/FuncionarioPublico.js
@@ -0,0 +1,13 @@
+var mongoose = require ('mongoose');
+
+var FPModel = new mongoose.Schema({
+    nombre:{
+        type: String,
+        unique: true,
+        index: true
+    },
+    partidoPolitico: String,
+    sueldo: String
+});
+
+module.exports = mongoose.model('FuncionarioPublicoM', FPModel);
